ae234a1fcc3f55c59861aba6e1711b49f649cab2
Guiznx <guizinfut02@gmail.com>
2026-04-05 12:27:19 -0400

    api/raw.js

--- api/raw.js
+++ api/raw.js
@@ -3,10 +3,14 @@ let scripts = {};
 export default function handler(req, res) {
   let { id } = req.query;
 
-  if (!scripts[id]) {
+  let encoded = scripts[id];
+
+  if (!encoded) {
     return res.send("-- script não encontrado");
   }
 
+  let decoded = Buffer.from(encoded, "base64").toString("utf-8");
+
   res.setHeader("Content-Type", "text/plain");
-  res.send(scripts[id]);
+  res.send(decoded);
 }
